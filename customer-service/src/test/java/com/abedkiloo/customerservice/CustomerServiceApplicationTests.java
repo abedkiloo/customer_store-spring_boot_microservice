912e02ce17dc00492cb76918de57c8112a5ded00
Abednego Wambua <abedxh@gmail.com>
2021-05-05 19:11:11 +0300

    customer serivice

---
+++ customer-service/src/test/java/com/abedkiloo/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abedkiloo.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
